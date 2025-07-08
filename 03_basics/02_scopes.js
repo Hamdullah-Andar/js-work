1091dce8b8105b8ddca7ac354d0f600fa58dc7d4
= <108467703+Hamdullah-Andar@users.noreply.github.com>
2025-07-08 10:20:25 +0430

    Remove commented console logs and enhance function examples in scope demonstration

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,12 +5,63 @@ let a = 300;
 
 if(true){
     let a = 10;
-    console.log("Inner : ", a)
+    // console.log("Inner : ", a)
     const b = 20;
     // var c = 30
     // What ever goes outside a scope is called global scope
 }
 
-console.log(a)
+// console.log(a)
 // console.log(b)
 // console.log(c)
+
+
+function one() {
+    const username = "Ahmad"
+
+    function two(){
+        const website = "My Website"
+        console.log(username)
+    }
+    // console.log(website)
+
+    // two()
+}
+
+// one()
+
+if(true){
+    const username = "Ahmad"
+    if(username === "Ahmad"){
+        const website = " myWebsite"
+        // console.log(username + website)
+    }
+    // console.log(website)
+}
+// console.log(username)
+
+
+// +++++++++++++++++++++++++++++++++ Intersting +++++++++++++++++++++++++++++++++
+
+console.log(addOne(8)) // addOne can be used before declaration, and it works fine
+// Because function declarations are hoisted — both the name and the function body are moved to the top of the scope during compilation.
+
+ function addOne(num){ // this is normal function
+    return num + 1;
+ }
+ addOne(8)
+
+//  Under the hood, JavaScript treats it like this:
+//  function addOne(num) {
+//   return num + 1;
+// }
+// console.log(addOne(8));
+
+
+
+//  addTwo(9) // calling a function before the definition as below will show error, "ReferenceError: Cannot access 'addTwo' before initialization"
+ const addTwo = function(num){ // addTwo had hold function value, which is sometime called expression
+    return num + 2;
+ }
+
+ addTwo(9)
